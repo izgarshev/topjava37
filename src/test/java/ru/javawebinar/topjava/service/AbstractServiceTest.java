2c5d8a5d436827f63994db4f7e0f7b0e31a3e9b9
izgarshev <izgarshev_p@mail.ru>
2026-08-27 00:51:24 +0300

    prepare to hw6

---
+++ src/test/java/ru/javawebinar/topjava/service/AbstractServiceTest.java
@@ -0,0 +1,39 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.rules.ExternalResource;
+import org.junit.rules.Stopwatch;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.ActiveDbProfileResolver;
+import ru.javawebinar.topjava.TimingRules;
+
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ActiveProfiles(resolver = ActiveDbProfileResolver.class)
+public abstract class AbstractServiceTest {
+
+    @ClassRule
+    public static ExternalResource summary = TimingRules.SUMMARY;
+
+    @Rule
+    public Stopwatch stopwatch = TimingRules.STOPWATCH;
+
+    //  Check root cause with AssertJ: https://github.com/junit-team/junit-framework/issues/2129#issuecomment-565712630
+    protected <T extends Throwable> void validateRootCause(Class<T> rootExceptionClass, Runnable runnable) {
+        assertThatExceptionOfType(Throwable.class)
+                .isThrownBy(runnable::run)
+                .withRootCauseInstanceOf(rootExceptionClass);
+    }
+}
